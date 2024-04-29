cf247c2e2894759a61c9e32123c2d4edf9a24ffa
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-04-29 15:46:58 +0000

    task 2 complete

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,14 +3,14 @@
 exec {'update':
   command => '/usr/bin/apt-get update',
 }
-package {'nginx':
+-> package {'nginx':
   ensure => 'present',
 }
-file_line { 'http_header':
+-> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-exec {'run':
+-> exec {'run':
   command => '/usr/sbin/service nginx restart',
 }
